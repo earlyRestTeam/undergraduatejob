88d7ab8b5daa4c1593ee4889c9e37d8e16f48d49
lonely-lyx <872352442@qq.com>
2019-12-17 17:31:37 +0800

    新项目

--- src/main/java/com/lyx/undergraduatejob/RunningApplication.java
+++ src/main/java/com/lyx/undergraduatejob/RunningApplication.java
@@ -1,14 +1,10 @@
 package com.lyx.undergraduatejob;
 
-
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@ComponentScan("com")
-@MapperScan("com.lyx.undergraduatejob.dao")
 public class RunningApplication {
     public static void main(String[] args) {
         SpringApplication.run(RunningApplication.class,args);
