8a94309fd1b51e22d540ceaa48a15e79798bd609
xxf <2953651905@qq.com>
2019-12-20 18:53:20 +0800

    后台搜索框

--- src/main/java/com/lyx/undergraduatejob/services/IAutStudentService.java
+++ src/main/java/com/lyx/undergraduatejob/services/IAutStudentService.java
@@ -3,17 +3,22 @@ package com.lyx.undergraduatejob.services;
 import com.github.pagehelper.PageInfo;
 import com.lyx.undergraduatejob.pojo.AutStudent;
 
+import java.util.List;
 import java.util.Map;
 
 public interface IAutStudentService {
 
-    PageInfo<AutStudent> queryAutStudent(Integer indexpage, AutStudent autStudent, String columnName, String sort);
+    PageInfo<AutStudent> queryAutStudentBefore(Integer indexpage,Integer userid, AutStudent autStudent, String columnName, String sort);
 
-    Map<String, String> updateAutStudent(AutStudent autStudent);
+    Map<String, String> updateAutStudentBefore(AutStudent autStudent, Integer userid);
 
-    Map<String, String>  addAutStudent(AutStudent autStudent);
+    Map<String, String>  addAutStudent(AutStudent autStudent, Integer userid);
 
     Map<String, String> deleteAutStudent(Integer id);
 
     boolean isAddAutStudent(Integer userid);
+
+    List<AutStudent> queryAutStudentBack(AutStudent autStudent);
+
+    Map<String, String> updateAutStudentBack(AutStudent autStudent);
 }
