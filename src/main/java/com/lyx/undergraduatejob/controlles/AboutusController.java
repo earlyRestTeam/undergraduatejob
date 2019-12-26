92efc34d031eb36cd59eb6494da0cce681ba4b41
yang199865 <2205348277@qq.com>
2019-12-26 10:06:27 +0800

    平台数据显示

--- src/main/java/com/lyx/undergraduatejob/controlles/AboutusController.java
+++ src/main/java/com/lyx/undergraduatejob/controlles/AboutusController.java
@@ -2,10 +2,14 @@ package com.lyx.undergraduatejob.controlles;
 
 import com.github.pagehelper.PageInfo;
 import com.lyx.undergraduatejob.pojo.Company;
+import com.lyx.undergraduatejob.pojo.Job;
+import com.lyx.undergraduatejob.pojo.Users;
 import com.lyx.undergraduatejob.search.entity.CompanySerchEntity;
 import com.lyx.undergraduatejob.search.entity.JobSearchEntity;
+import com.lyx.undergraduatejob.search.entity.UsersSearchEntity;
 import com.lyx.undergraduatejob.services.ICompanyInfoServices;
 import com.lyx.undergraduatejob.services.IJobServices;
+import com.lyx.undergraduatejob.services.IUserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.Null;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -23,12 +29,33 @@ public class AboutusController {
     @Autowired
     ICompanyInfoServices iCompanyInfoServices;
 
+    @Autowired
+    IUserServices userServices;
+
     @RequestMapping("toaboutus")
-    public String toaboutus(){
+    public String toaboutus( HttpServletRequest request){
         JobSearchEntity jobSearchEntity = new JobSearchEntity();
-        jobSearchEntity.setKeyWord("status");
-//        iJobService.selectJobByJobSearchEntity(1,1,jobSearchEntity);
-        return "";
+        PageInfo<Job> jobPageInfo = iJobService.selectJobByJobSearchEntityWithOutCompany(1, 10, jobSearchEntity);
+        Long jobnNums =  jobPageInfo.getTotal();
+
+        UsersSearchEntity usersSearchEntity = new UsersSearchEntity();
+        usersSearchEntity.setStatus(0);
+        PageInfo<Users> usersPageInfo = userServices.queryUsers(1, 10, usersSearchEntity);
+        long userNums = usersPageInfo.getTotal();
+
+        CompanySerchEntity companySerchEntity = new CompanySerchEntity();
+        companySerchEntity.setAulStatus(1);
+        companySerchEntity.setStatus(1);
+        PageInfo info = iCompanyInfoServices.queryCompanyListByAdmin(1, companySerchEntity);
+        long companyNums = info.getTotal();
+
+        Map<String, Long> map = new HashMap<>();
+        map.put("jobnNums",jobnNums);
+        map.put("userNums",userNums);
+        map.put("companyNums",companyNums);
+
+        request.setAttribute("result",map);
+        return "/about_us";
 
     }
 
